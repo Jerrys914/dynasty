24f8ddeb8b440ea7f53b9f4f487514fad88e540b
Jerry Shanahan <jerry.shanahan@gmail.com>
2017-04-21 19:28:06 -0400

    production test

--- server/db/config.js
+++ server/db/config.js
@@ -1,9 +1,9 @@
 const knex = require('knex')({
   client: 'mysql',
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: 'dynasty',
     port: 3306
   }, 
